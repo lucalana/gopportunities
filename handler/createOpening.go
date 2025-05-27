73982d5198944c66f97526ac1597ce71de79a887
lucalana <lucalana13@gmail.com>
2025-05-27 14:59:15 -0300

    feat: create request and response to create opening handler

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -4,10 +4,34 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lucalana/gopportunities/schemas"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "POST Opening",
-	})
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Validation error %v", err.Error())
+		sendError(ctx, 404, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("Error creating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error creating opening")
+		return
+	}
+
+	sendSuccess(ctx, "create-opening", opening)
 }
